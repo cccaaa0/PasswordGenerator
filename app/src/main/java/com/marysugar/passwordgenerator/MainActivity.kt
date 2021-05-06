02100664557fe81ff8d2720d2f6df0f151326aec
cccaaa0 <iseekana@gmail.com>
2021-05-06 13:10:22 +0900

    Implementation of a function to specify the number and type of characters.

--- app/src/main/java/com/marysugar/passwordgenerator/MainActivity.kt
+++ app/src/main/java/com/marysugar/passwordgenerator/MainActivity.kt
@@ -4,13 +4,10 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import androidx.recyclerview.widget.RecyclerView
+import org.apache.commons.lang3.RandomStringUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +15,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val passwordList = ArrayList<String>()
+        var numberOfCharacters = 8
+        var passwordType = "alphanumeric"
 
         (1..12).forEach { _ ->
-            passwordList.add(generatePassword())
+            passwordList.add(generatePassword(numberOfCharacters, passwordType))
         }
 
         val gridView = findViewById<GridView>(R.id.gv_passwords)
@@ -34,32 +33,49 @@ class MainActivity : AppCompatActivity() {
         gridView.adapter = adapter
         gridView.onItemClickListener = ListItemClickListener()
 
-        val checkBox = findViewById<CheckBox>(R.id.cb_alphanumeric_char)
-        checkBox.isChecked = true
+        val rgPasswordType = findViewById<RadioGroup>(R.id.rg_password_type)
+        rgPasswordType.check(R.id.rb_alphanumeric)
 
-        val radioGroup = findViewById<RadioGroup>(R.id.rg_num_char)
-        radioGroup.check(R.id.rb_8_char)
-
-        radioGroup.setOnCheckedChangeListener { _, checkId ->
+        rgPasswordType.setOnCheckedChangeListener { _, checkId ->
             val radioButton = findViewById<RadioButton>(checkId)
-            Toast.makeText(this, radioButton.text, Toast.LENGTH_SHORT).show()
+            passwordType = when (radioButton.text) {
+                getString(R.string.rb_alphanumeric) -> "alphanumeric"
+                getString(R.string.rb_alphanumeric_and_symbol) -> "alphanumericAndSymbol"
+                else -> "alphanumeric"
+            }
+        }
+
+        val rgNumOfChar = findViewById<RadioGroup>(R.id.rg_num_of_char)
+        rgNumOfChar.check(R.id.rb_8_char)
+
+        rgNumOfChar.setOnCheckedChangeListener { _, checkId ->
+            val radioButton = findViewById<RadioButton>(checkId)
+            numberOfCharacters = when (radioButton.text) {
+                getString(R.string.rb_8_char) -> 8
+                getString(R.string.rb_12_char) -> 12
+                getString(R.string.rb_16_char) -> 16
+                else -> 8
+            }
         }
 
         val button = findViewById<Button>(R.id.bt_generate)
         button.setOnClickListener {
             passwordList.clear()
             (1..12).forEach { _ ->
-                passwordList.add(generatePassword())
+                passwordList.add(generatePassword(numberOfCharacters, passwordType))
             }
             adapter.notifyDataSetChanged()
         }
     }
 
-    private fun generatePassword() : String {
-        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
-        return (1..8)
-                .map { allowedChars.random() }
-                .joinToString("")
+    private fun generatePassword(numberOfCharacters: Int, passwordType: String) : String {
+        if (passwordType == "alphanumeric") {
+            return RandomStringUtils.randomAlphanumeric(numberOfCharacters)
+        }
+        if (passwordType == "alphanumericAndSymbol") {
+            return RandomStringUtils.randomAscii(numberOfCharacters)
+        }
+        return "Error"
     }
 
     private inner class ListItemClickListener : AdapterView.OnItemClickListener {
@@ -67,12 +83,10 @@ class MainActivity : AppCompatActivity() {
             val item = parent?.getItemAtPosition(position)
 
             val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            // Creates a new text clip to put on the clipboard
-            val clip: ClipData = ClipData.newPlainText("simple text", item.toString())
-            // Set the clipboard's primary clip.
+            val clip: ClipData = ClipData.newPlainText("password", item.toString())
             clipboard.setPrimaryClip(clip)
 
-            Toast.makeText(this@MainActivity, "Copy", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@MainActivity, getString(R.string.cb_copy), Toast.LENGTH_SHORT).show()
         }
     }
 }
